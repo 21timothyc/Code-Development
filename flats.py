c38b386d49858df0c22a242a24722010b5c3530e
21timothyc <timothyclason@gmail.com>
2017-09-05 18:18:07 -0600

    Create flats.py

---
+++ flats.py
@@ -0,0 +1,10 @@
+from telescope import ssh_telescope
+
+class autoFlat(self,exptime, bin, filter, count):
+    if exptime < 60
+        exptime = 60
+        print "Your exposure time is to long-set to 60"
+        time.sleep(1)
+    else
+        print "Connection to telescope"
+    connect()
