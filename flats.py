af3a43f47a312430b4f81c7323f38561daa61802
21timothyc <timothyclason@gmail.com>
2017-09-09 18:59:42 -0600

    Update flats.py

--- flats.py
+++ flats.py
@@ -1,7 +1,7 @@
 class flatSet(object):
     count = 3
     bin = 2
-    filter = ["u-band","g-band","r-band","etc"]
+    filter = "g-band"
     #u-band, g-band, r-band, i-band, z-band, clear, or h-alpha
 
     def __init__(self, bin, filter, count):
@@ -9,3 +9,9 @@ class flatSet(object):
         self.bin = bin
         self.filter = filter
         self.count = count
+    
+    def toString(self) -> str:
+        return 'This set of flats has a count of %d, binning of %s, and filter is set at %s.'%(self.count, self.bin, self.filter)
+
+fs = flatSet(bin,filter,count)
+print(fs.toString())
